567189dd468412dcec5fe0a28390277797076ece
Lee Aylward <lee@laylward.com>
2012-03-24 23:11:55 -0500

    added shared HTTPD and jenkins plugin

---
+++ lib/Fuckbot/HTTPD.pm
@@ -0,0 +1,13 @@
+use v5.14;
+
+package Fuckbot::HTTPD 0.1 {
+  use AnyEvent::HTTPD;
+  my $httpds = {}; # shared between instances
+
+  sub new {
+    my ($class, $port) = @_;
+    $httpds->{$port} ||= AnyEvent::HTTPD->new(port => $port);
+  }
+}
+
+1;
--- lib/Fuckbot/Plugin/Jenkins.pm
+++ lib/Fuckbot/Plugin/Jenkins.pm
@@ -1,31 +1,26 @@
 use v5.14;
 
-package Fuckbot::Plugin::Github 0.1 {
+package Fuckbot::Plugin::Jenkins 0.1 {
   use parent 'Fuckbot::Plugin';
   use Fuckbot::HTTPD;
   use JSON::XS;
-
+  
   sub prepare_plugin {
     my $self = shift;
     my $port = $self->config("port") || 9091;
     $self->{httpd} = Fuckbot::HTTPD->new($port);
-    $self->{httpd}->reg_cb("/github" => sub { $self->handle_req(@_) });
+    $self->{httpd}->reg_cb("/jenkins" => sub { $self->handle_req(@_) });
   }
 
   sub handle_req {
     my ($self, $httpd, $req) = @_;
 
-    $req->respond({ content => ["text/plain", "o ok"] });
+    $self->respond({ content => ["text/plain", "o ok"] });
     my $payload = $req->parm("payload");
 
     if ($payload) {
       my $data = decode_json $payload;
-      my $repo = $data->{repository}{name};
-      my @commits = map {
-        "[$repo] $_->{message} ($_->{author}{name}) $_->{url}"
-      } @{$data->{commits}};
-
-      $self->broadcast(@commits);
+      $self->broadcast("build #$data{number} of job has $build{phase} $build{status}");
     }
   }
 }
