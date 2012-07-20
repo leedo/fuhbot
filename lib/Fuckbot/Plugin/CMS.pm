9f3e40aea81bff649ac3577f6b50e8e32c208eb1
Lee Aylward <lee@laylward.com>
2012-07-20 13:46:54 -0500

    added CMS plugin

---
+++ lib/Fuckbot/Plugin/CMS.pm
@@ -0,0 +1,29 @@
+use v5.14;
+
+package Fuckbot::Plugin::CMS 0.1 {
+  use parent 'Fuckbot::Plugin';
+  use Fuckbot::HTTPD;
+  use JSON::XS;
+  
+  sub prepare_plugin {
+    my $self = shift;
+    my $port = $self->config("port") || 9091;
+    $self->{httpd} = Fuckbot::HTTPD->new($port);
+    $self->{httpd}->reg_cb("/cms" => sub { $self->handle_req(@_) });
+  }
+
+  sub handle_req {
+    my ($self, $httpd, $req) = @_;
+
+    $req->respond({ content => ["text/plain", "o ok"] });
+    my $payload = $req->parm("payload");
+
+    if ($payload) {
+      my $data = decode_json $payload;
+      my $color = $data->{type} eq "error" ? 4 : 3;
+      $self->broadcast("\x03$color\x02CMS $data->{type}:\x02\x03 $data->{message} $data->{url}");
+    }
+  }
+}
+
+1;
