6465ff0f5bb8df0b7bfeec3ec276f1d8b51a0df3
Lee Aylward <lee@laylward.com>
2012-09-18 22:29:45 -0500

    clean up Seen plugin

--- lib/Fuhbot/Plugin/Seen.pm
+++ lib/Fuhbot/Plugin/Seen.pm
@@ -3,25 +3,37 @@ use v5.14;
 package Fuhbot::Plugin::Seen  0.1 {
   use parent 'Fuhbot::Plugin';
 
+  use AnyEvent::IRC::Util ();
+  use JSON::XS ();
+
   sub commands {qw/seen/}
 
   sub irc_privmsg {
     my ($self, $irc, $msg) = @_;
     my $chan = $msg->{params}[0];
-    my ($nick) = split_prefix $msg->{prefix};
-    $self->brain->set(join "-", $nick, $chan, $irc->name, sub {});
+    my ($nick) = AnyEvent::IRC::Util::split_prefix $msg->{prefix};
+    my $key = join "-", $nick, $chan, $irc->name;
+    $self->brain->set($key, JSON::XS::encode_json [time, $msg->{params}[-1]], sub {});
   }
 
   sub seen {
     my ($self, $irc, $chan, $nick) = @_;
-    $self->brain->get(join "-", $nick, $chan, $irc->name, sub {
+    my $key = join "-", $nick, $chan, $irc->name;
+    $self->brain->get($key, sub {
       my ($data) = @_;
+
       if (!$data) {
         $irc->send_srv(PRIVMSG => $chan, "$nick has not been seen in $chan");
+        return;
       }
-      my ($time, $message) = decode_json $data;
-      my $diff = time - $time;
-      $irc->send_srv(PRIVMSG => $chan, "$nick was last seen in $chan $diff seconds ago: $message");
+
+      my ($time, $message) = @{ JSON::XS::decode_json $data };
+      my @date = (localtime($time))[4,3,5,2,1];
+      $date[2] += 1900;
+      my $when = sprintf "on %02d/%02d/%02d at %02d:%02d", @date;
+
+      $irc->send_srv(PRIVMSG => $chan, "$nick was last seen in $chan $when");
+      $irc->send_srv(PRIVMSG => $chan, " < $nick> $message");
     });
   }
 }
