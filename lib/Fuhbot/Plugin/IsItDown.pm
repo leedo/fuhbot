2ffc79bcf7baefbd1998f4fd4368a39685698e10
Lee Aylward <lee@laylward.com>
2013-01-03 12:29:29 -0600

    support HTTP auth in IsItDown

--- lib/Fuhbot/Plugin/IsItDown.pm
+++ lib/Fuhbot/Plugin/IsItDown.pm
@@ -3,6 +3,7 @@ package Fuhbot::Plugin::IsItDown;
 use parent 'Fuhbot::Plugin';
 
 use AnyEvent::HTTP;
+use Mime::Base64 ();
 
 sub commands {
   my $self = shift;
@@ -21,8 +22,15 @@ sub commands {
 
 sub isitdown {
   my ($self, $irc, $chan, $site) = @_;
+  my %headers;
+
+  if ($site =~ s{^https?://([^:]+:[^:]+)@}{}) {
+    $headers{Authorization} = "Basic " . MIME::Base64::encode_base64($1);
+  }
+
   $site = "http://$site" unless $site =~ m{^https?://};
-  http_get $site, sub {
+
+  http_get $site, headers => \%headers, sub {
     my (undef, $headers) = @_;
     my $state = $headers->{Status} == 200 ? "up" : "down ($headers->{Reason})";
     $irc->send_srv(PRIVMSG => $chan, "$site is $state");
