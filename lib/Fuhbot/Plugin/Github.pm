5fe4efe96b2763a4289556d669c59630d88c5143
Lee Aylward <lee@laylward.com>
2013-06-27 15:43:05 -0400

    update !github status to use new(ish) API

--- lib/Fuhbot/Plugin/Github.pm
+++ lib/Fuhbot/Plugin/Github.pm
@@ -9,36 +9,17 @@ package Fuhbot::Plugin::Github 0.1 {
   on command "github status" => sub {
     my ($self, $irc, $chan) = @_;
 
-    my %headings = (
-      'good' => "\x033Battle station fully operational",
-      'minorproblem' => "\x037Partial service outage",
-      'majorproblem' => "\x034Major service outage",
+    my %colors = (
+      'good' => "\x033",
+      'minorproblem' => "\x037",
+      'majorproblem' => "\x034",
     ); 
 
-    http_get "https://status.github.com/status.json" => sub {
+    http_get "https://status.github.com/api/last-messages.json" => sub {
       my ($body, $headers) = @_;
       if ($headers->{Status} == 200) {
         my $data = decode_json $body;
-        $irc->send_srv(PRIVMSG => $chan, $headings{$data->{status}});
-        for my $day (@{$data->{days}}) {
-          if ($day->{date} eq "Today") {
-            my @msgs = map  {s/<[^>]+>/ /g; $_}
-                       grep {$_}
-                       split "\n", $day->{message};
-            $irc->send_srv(PRIVMSG => $chan, $_) for @msgs;
-          }
-        }
-      }
-    };
-
-    http_get "https://status.github.com/realtime.json" => sub {
-      my ($body, $headers) = @_;
-      if ($headers->{Status} == 200) {
-        my $data = decode_json $body;
-        my $msg = join " \x03| ", map {
-          ($data->{$_} ? "\x033" : "\x034") . $_;
-        } keys %$data;
-        $irc->send_srv(PRIVMSG => $chan, $msg);
+        $irc->send_srv(PRIVMSG => $chan, $colors{$data->{status}} . $data->{body});
       }
     };
   };
