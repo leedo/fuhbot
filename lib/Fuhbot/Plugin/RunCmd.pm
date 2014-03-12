f05137b36487b14587ff91166590fe11ae6efdf5
Lee Aylward <lee@laylward.com>
2014-03-12 11:48:26 -0400

    add a RunCmd plugin
    will be used to add some knife commands

---
+++ lib/Fuhbot/Plugin/RunCmd.pm
@@ -0,0 +1,39 @@
+use v5.14;
+
+package Fuhbot::Plugin::RunCmd 0.1 {
+  use Fuhbot::Plugin;
+  use AnyEvent::Util;
+
+  sub prepare_plugin {
+    my $self = shift;
+    $self->{jobs} = {};
+
+    if (my $commands = $self->config("commands")) {
+      for my $command (@$commands) {
+        my ($name, $pattern, $line) = @$command;
+        on command $pattern => sub {
+          my ($self, $irc, $chan, @args) = @_;
+          if ($self->{jobs}{$name}) {
+            return $self->broadcast("$name is already running");
+          }
+
+          $self->{jobs}{$name};
+
+          $self->broadcast("starting $name");
+          $self->{jobs}{$name} = run_cmd sprintf($line, @args),
+            '>' => sub {
+              my @lines = grep {$_} split qr{\015?\012}, $_[0];
+              $irc->send_srv(PRIVMSG => $chan, $_) for @lines;
+            };
+
+          $self->{jobs}{$name}->cb(sub {
+            $self->broadcast("$name completed");
+            delete $self->{jobs}{$name};
+          });
+        };
+      }
+    }
+  }
+}
+
+1;
