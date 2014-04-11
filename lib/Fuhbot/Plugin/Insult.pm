403058753563bf2792c4695a365681b4db7bd1fc
Lee Aylward <lee@laylward.com>
2014-04-11 19:11:47 -0400

    add !praise to insult plugin

--- lib/Fuhbot/Plugin/Insult.pm
+++ lib/Fuhbot/Plugin/Insult.pm
@@ -4,8 +4,8 @@ package Fuhbot::Plugin::Insult 0.1 {
   use Fuhbot::Plugin;
   use Encode;
 
-  on command qr{insult\s*(.*)} => sub {
-    my ($self, $irc, $chan, $nick) = @_;
+  on command qr{(insult|praise)\s*(.*)} => sub {
+    my ($self, $irc, $chan, $action, $nick) = @_;
     if (!$nick) {
       my @nicks = keys %{$irc->channel_list($chan) || {}};
       return unless @nicks;
@@ -13,15 +13,15 @@ package Fuhbot::Plugin::Insult 0.1 {
     }
     $nick =~ s/^\s+//;
     $nick =~ s/\s+$//;
-    $self->brain->srandmember("insults", sub {
-      my $insult = $_[0] || "I don't have an insult";
+    $self->brain->srandmember($action . "s", sub {
+      my $insult = $_[0] || "I don't have an $action";
       $irc->send_srv(PRIVMSG => $chan, "hey $nick, $insult");
     });
   };
 
-  on command qr{add insult\s+(.+)} => sub {
-    my ($self, $irc, $chan, $insult) = @_;
-    $self->brain->sadd("insults", $insult, sub {
+  on command qr{add (insult|praise)\s+(.+)} => sub {
+    my ($self, $irc, $chan, $action, $insult) = @_;
+    $self->brain->sadd($action . "s", $insult, sub {
       $irc->send_srv(PRIVMSG => $chan, "ok!");
     });
   };
