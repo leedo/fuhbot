994b2b2129312caef595bf8636e096f09929eff2
Lee Aylward <lee@laylward.com>
2015-12-22 12:13:44 -0500

    remove warnings

--- lib/Fuhbot/Plugin/Cron.pm
+++ lib/Fuhbot/Plugin/Cron.pm
@@ -3,19 +3,15 @@ package Fuhbot::Plugin::Cron 0.1 {
   use DateTime::Event::Cron;
 
   sub prepare_plugin ($self) {
-    warn "setting up timer";
     $self->{timer} = AE::timer 0, 60, $self->cron;
   }
 
   sub cron ($self) {
     return sub {
-      warn "running cron";
       my $jobs = $self->config("jobs");
       for my $job (@$jobs) {
         my $c = DateTime::Event::Cron->new($job->[0]);
-        warn $job->[0];
         if ($c->match) {
-          warn "match";
           $job->[1]($self);
         }
       }
