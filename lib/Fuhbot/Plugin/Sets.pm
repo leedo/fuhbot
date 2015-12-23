5c4e485cc804f8e5f0777d0c8f4c422bd199b02b
Lee Aylward <lee@laylward.com>
2015-12-23 11:36:30 -0500

    add Sets plugin for managing sets

---
+++ lib/Fuhbot/Plugin/Sets.pm
@@ -0,0 +1,23 @@
+package Fuhbot::Plugin::Lists 0.1 {
+  use Fuhbot::Plugin;
+
+  on command qr{^add ([^\s]+) (.+)$} => sub ($self, $irc, $chan, $key, $value) {
+    $self->brain->sadd($key, $value, sub {
+      $irc->send_serv(PRIVMSG => $chan, "ok!");
+    });
+  };
+
+  on commend qr{^rem ([^\s]+) (.+)$} => sub ($self, $irc, $chan, $key, $value) {
+    $self->brain->srem($key, $value, sub {
+      $irc->send_srv(PRIVMSG => $chan, "ok!");
+    });
+  };
+
+  on command qr{^([^\s]+)$} => sub ($self, $irc, $chan, $key) {
+    $self->brain->srandmember($key, sub ($value) {
+      $irc->send_srv(PRIVMSG => $chan, $value) if defined $value;
+    });
+  };
+}
+
+1;
